7c29fe65c093113ae52e5543d42ab2fb368c8c0f
Kristof personal <kristof.berge@gmail.com>
2018-03-18 22:11:52 -0300

    [Android] Add GLSurfaceView to ARPage

---
+++ TDCARExample/TDCARExample.Android/PageRenderers/ARPageRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TDCARExample.Droid.PageRenderers;
+using TDCARExample;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Opengl;
+
+[assembly: ExportRenderer(typeof(ARPage), typeof(ARPageRenderer))]
+namespace TDCARExample.Droid.PageRenderers
+{
+    public class ARPageRenderer : PageRenderer
+    {
+        private GLSurfaceView surfaceView;
+
+        private Activity Activity => Context as Activity;
+
+        public ARPageRenderer(Context context) : base (context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+            
+            Activity.SetContentView(Resource.Layout.ARView);
+
+            this.surfaceView = Activity.FindViewById<GLSurfaceView>(Resource.Id.surfaceview);
+        }
+    }
+}
